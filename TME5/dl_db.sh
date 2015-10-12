3590f1a85b917f39a854b9f5d751aee5802de3ab
CADENE REMI <3000693@ppti-14-502-06.ufr-info-p6.jussieu.fr>
2015-10-12 22:22:10 +0200

    TME5 remi sigma

---
+++ TME5/dl_db.sh
@@ -0,0 +1,8 @@
+wget http://files.grouplens.org/datasets/movielens/ml-100k.zip
+unzip ml-100k.zip
+mv ml-100k data100k
+rm ml-100k.zip
+wget http://files.grouplens.org/datasets/movielens/ml-1m.zip
+unzip ml-1m.zip
+mv ml-1m data1m
+rm ml-1m.zip
