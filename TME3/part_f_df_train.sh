d939df7767e65be4b2624f16e88f965a6c20c2a7
CHEN MICKAEL <3152691@ppti-14-502-05.ufr-info-p6.jussieu.fr>
2015-11-23 20:50:14 +0100

    Multithreading Kaggle

--- TME3/part_f_df_train.sh
+++ TME3/part_f_df_train.sh
@@ -6,7 +6,8 @@ function process_parts {
     cd ~/FDMS/TME3
     for part in `seq $1 $2`
     do
-	nohup python f_df_train.py $part > f_df_train_$part.log &
+	echo "Starting part ${part} on $HOSTNAME"
+	nohup python f_df_train.py ${part} > f_df_train_${part}.log &
     done
 }
 
